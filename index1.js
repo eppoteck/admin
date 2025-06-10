84d01e72a3a7dc7e0269c5ce7c2f7dff6c94731b
eppoteck <rifqirifky2@gmail.com>
2025-06-10 10:46:51 +0700

    Create index1.js

---
+++ index1.js
@@ -0,0 +1,54 @@
+const sideMenu = document.querySelector('aside');
+const menuBtn = document.getElementById('menu-btn');
+const closeBtn = document.getElementById('close-btn');
+
+const darkMode = document.querySelector('.dark-mode');
+
+menuBtn.addEventListener('click', () => {
+    sideMenu.style.display = 'block';
+});
+
+closeBtn.addEventListener('click', () => {
+    sideMenu.style.display = 'none';
+});
+
+darkMode.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode-variables');
+    darkMode.querySelector('span:nth-child(1)').classList.toggle('active');
+    darkMode.querySelector('span:nth-child(2)').classList.toggle('active');
+})
+
+
+Orders.forEach(order => {
+    const tr = document.createElement('tr');
+    const trContent = `
+        <td>${order.productName}</td>
+        <td>${order.productNumber}</td>
+        <td>${order.paymentStatus}</td>
+        <td class="${order.status === 'Declined' ? 'danger' : order.status === 'Pending' ? 'warning' : 'primary'}">${order.status}</td>
+        <td class="primary">Details</td>
+    `;
+    tr.innerHTML = trContent;
+    document.querySelector('table tbody').appendChild(tr);
+
+  const Orders = [
+    {
+        productName: 'luna',
+        productNumber: '100',
+        paymentStatus: 'sudah',
+        status: 'Active'
+    },
+    {
+        productName: 'faren',
+        productNumber: '50',
+        paymentStatus: 'sudah',
+        status: 'Active'
+    },
+    {
+        productName: 'ardani',
+        productNumber: '50',
+        paymentStatus: 'sudah',
+        status: 'Active'
+    },
+]
+});
