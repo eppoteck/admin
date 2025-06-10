f30093368831efd64c577e8c6a54dcda84ae6edc
eppoteck <rifqirifky2@gmail.com>
2025-06-10 10:48:40 +0700

    Update index1.js

--- index1.js
+++ index1.js
@@ -30,25 +30,4 @@ Orders.forEach(order => {
     `;
     tr.innerHTML = trContent;
     document.querySelector('table tbody').appendChild(tr);
-
-  const Orders = [
-    {
-        productName: 'luna',
-        productNumber: '100',
-        paymentStatus: 'sudah',
-        status: 'Active'
-    },
-    {
-        productName: 'faren',
-        productNumber: '50',
-        paymentStatus: 'sudah',
-        status: 'Active'
-    },
-    {
-        productName: 'ardani',
-        productNumber: '50',
-        paymentStatus: 'sudah',
-        status: 'Active'
-    },
-]
 });
