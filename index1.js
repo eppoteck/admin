84c9353369d2bd7ecc4604cd86f4c922ba6eaad5
eppoteck <rifqirifky2@gmail.com>
2025-06-10 11:04:31 +0700

    Add files via upload

---
+++ index1.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: 'lunaprisca',
+        productNumber: '100',
+        paymentStatus: '10-6-2025',
+        status: 'Active'
+    },
+    {
+        productName: 'afaren',
+        productNumber: '50',
+        paymentStatus: '10-6-2025',
+        status: 'Active'
+    },
+    {
+        productName: 'ardani',
+        productNumber: '50',
+        paymentStatus: '10-6-2025',
+        status: 'Active'
+    },
+]
